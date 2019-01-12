6135ff4ecf26345ea41440ceaa119316ac1212f9
gus5298 <gsanchezcollado@gmail.com>
2019-01-12 13:46:31 +0100

    Building 9, view for floor 1st created

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import startup from './screens/Startup';
 import signin from './screens/Signin';
 import signup from './screens/Signup';
 import settings from './screens/Settings';
+import building from './screens/Building';
 import termsAndConditions from './screens/TermsAndContidions';
 import map from './screens/Map';
+
+
+
 
 const App = createStackNavigator({
     home: {screen: startup,navigationOptions: () => ({
@@ -17,6 +21,7 @@ const App = createStackNavigator({
     })},
   signup: {screen: signup,navigationOptions: () => ({title:'CampusFinder', headerBackTitle:'Back'})},
   settings: {screen: settings,navigationOptions: () => ({title:'Campusfinder', headerBackTitle:'Back'})},
+  building: {screen: building,navigationOptions: () => ({title:'Campusfinder', headerBackTitle:'Back'})},
   termsAndConditions: {screen: termsAndConditions,navigationOptions: () => ({title:'Campusfinder', headerBackTitle:'Back'})},
   map: {screen: map,navigationOptions: () => ({title:'CampusFinder', headerBackTitle:'Back'})},
 });
