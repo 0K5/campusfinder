260d85c5d96bb8c6b329d6e96fbd4da3e6ea76ae
Silvester <23085539+0K5@users.noreply.github.com>
2019-01-15 21:35:20 +0100

    Update RestSaver.js
    Updated saver for data persistence.

--- services/RestSaver.js
+++ services/RestSaver.js
@@ -8,8 +8,7 @@ export const saveData = async function(cb, storageKey, data){
         if(itemString){
             item = JSON.parse(itemString);
             if(item){
-                if(data && data instanceof Object)
-                for (let key in data){
+                for (var key in data){
                     if(item.hasOwnProperty(key)){
                         item[key] = data[key];
                     }
@@ -18,19 +17,16 @@ export const saveData = async function(cb, storageKey, data){
                 .then(stored => {
                     let restUrl = Urls[storageKey];
                     if(restUrl){
-                        endpointCall(cb, restUrl, item);
+                        return endpointCall(cb, restUrl, item);
                     }else{
-                        cb({'errorcode': '1311', 'error':'Data saved to AsyncStorage but not to the Server. The storage key is not present in /constants/Urls as a key for a restCall'})
+                        return cb({'errorcode': '1311', 'error':'Data saved to AsyncStorage but not to the Server. The storage key is not present in /constants/Urls as a key for a restCall'})
                     }
-                }).catch(error => cb({'errorcode': '1312', 'error':error.message}));
-                }else{
-                    cb({'errorcode': '1313', 'error':'No valid data. Please use JS Object. (JSON)'})
-                }
+                }).catch(error => console.log(error.message));
             }else{
-                cb({'errorcode': '1314', 'error':'Storage item not JSON parseable'})
+                return cb({'errorcode': '1314', 'error':'Storage item not JSON parseable'})
             }
         }else{
-            cb({'errorcode': '1315', 'error':'Not a valid storage item'})
+            return cb({'errorcode': '1315', 'error':'Not a valid storage item'})
         }
-     }).catch(error => cb({'errorcode': '1316', 'error':error.message}));
+     }).catch(error => console.log(error.message));
 }
