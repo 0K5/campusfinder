1233872e468d2d0d617154a75b64e8c7a54c6ca2
Silvester <23085539+0K5@users.noreply.github.com>
2019-01-16 18:24:58 +0100

    Update Startup.js
    Added cleaner redirect on Startup

--- screens/Startup.js
+++ screens/Startup.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { AsyncStorage, ActivityIndicator } from 'react-native';
+import { AsyncStorage, ActivityIndicator, Image } from 'react-native';
 import { prevAuthCall, endpointCall } from '../services/Rest';
 import Urls from '../constants/Urls';
 import {loadFromRest} from '../services/RestLoader';
-import SignIn from '../screens/Signin';
-import Map from '../screens/Map';
-import Settings from '../screens/Settings';
+
 
 export default class AppContainer extends Component {
 
@@ -29,6 +27,10 @@ export default class AppContainer extends Component {
         this.restLoad();
     }
 
+    navToNext(next){
+        return this.props.navigation.navigate(next);
+    }
+
     render() {
         if (!this.state.isLoaded) {
             return (
@@ -37,11 +39,15 @@ export default class AppContainer extends Component {
         }else{
             if(!this.state.keyValid){
                 return(
-                    <SignIn {...this.props}/>
+                    <Image
+                        onLoad={() => this.navToNext('signin')}
+                    ></Image>
                 );
             }else{
                 return(
-                    <Settings {...this.props}/>
+                    <Image
+                        onLoad={() => this.navToNext('settings')}
+                    ></Image>
                 );
             }
         }
